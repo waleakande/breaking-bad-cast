f2cb9feb260e2f7ab17ef93356b53c5aa17df1dc
Olawale Akande <walemicheal4@gmail.com>
2022-03-27 21:46:36 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/ui/Header'
+import CharacterGrid from './components/characters/CharacterGrid'
+import Search from './components/ui/Search';
+import Pagination from './components/ui/Pagination';
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(12);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      setIsLoading(true);
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`)
+
+      setItems(result.data);
+      setIsLoading(false);
+    }
+
+    fetchItems();
+  }, [query]);
+
+  // Get current items
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = items.slice(indexOfFirstItem, indexOfLastItem);
+
+  // Change page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header />
+      <Search getQuery={setQuery} placeholder="Search for Characters..." />
+      <CharacterGrid isLoading={isLoading} items={currentItems} />
+      <Pagination 
+        itemsPerPage={itemsPerPage} 
+        totalItems={items.length} 
+        paginate={paginate} />
     </div>
-  );
+  )
 }
 
 export default App;
